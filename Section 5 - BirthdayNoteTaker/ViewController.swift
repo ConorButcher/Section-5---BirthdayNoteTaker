102e2a0ab991be4aeab2a09f29fdefc8dd2d2381
Conor Butcher <conorbutcher22@outlook.com>
2024-01-08 09:44:34 +0000

    Initial Commit 2

---
+++ Section 5 - BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Section 5 - BirthdayNoteTaker
+//
+//  Created by Conor Butcher on 08/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        UserDefaults.standard.setValue(nameTextField.text!, forKey: "name")
+        UserDefaults.standard.setValue(birthdayTextField.text!, forKey: "birthday")
+        
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday: \(birthdayTextField.text!)"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String {
+            nameLabel.text = newName
+        }
+        if let newBirthday = storedBirthday as? String {
+            birthdayLabel.text = newBirthday
+        }
+        
+        
+    }
+
+
+}
+
